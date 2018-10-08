5d8d7e83027c677b0ada47757fbb13a918d05ed0
3A532090 <raxttc@gmail.com>
2018-10-08 21:21:38 +0800

    1.3

--- src/Demo/Hello/Lara.php
+++ src/Demo/Hello/Lara.php
@@ -1,14 +1,14 @@
 <?php
-namespace Demo;
+namespace Demo\Hello;
 
-class HelloWorld{
+class Lara{
     function __construct()
     {
-        echo 'HelloWorld~'.PHP_EOL;
+        echo 'Hello Lara~'.PHP_EOL;
     }
 }/**
  * Created by PhpStorm.
  * User: M510
  * Date: 2018/10/8
- * Time: 下午 09:15
+ * Time: 下午 09:19
  */
