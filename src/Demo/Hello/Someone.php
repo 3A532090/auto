93bb0a2702415ec899ef93f8cd5ea940ed6c9423
3A532090 <raxttc@gmail.com>
2018-10-08 21:23:12 +0800

    1.4

---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,15 @@
+<?php
+namespace Demo\Hello;
+
+class Someone{
+    function __construct($name)
+    {
+        echo 'Hello '.$name.PHP_EOL;
+    }
+}
+/**
+ * Created by PhpStorm.
+ * User: M510
+ * Date: 2018/10/8
+ * Time: 下午 09:22
+ */
