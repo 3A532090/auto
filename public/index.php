ba3b5909164e9e3b18f5c2440aab6bcd9ee5a183
3A532090 <raxttc@gmail.com>
2018-10-08 21:29:00 +0800

    1.5

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+require '../vendor/autoload.php';
+
+use \Demo\HelloWorld as World;
+use Demo\Hello\Lara;
+use Demo\Hello;
+
+$world = new World();
+$lara= new Lara();
+$vincent= new Hello\Someone('Vincent');
+
+$mary = new \Demo\Hello\Someone('Mary');
+$john = new Demo\Hello\Someone('John');
+
+/**
+ * Created by PhpStorm.
+ * User: M510
+ * Date: 2018/10/8
+ * Time: 下午 09:27
+ */
